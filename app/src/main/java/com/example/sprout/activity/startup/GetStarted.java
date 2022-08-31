6df9872b2e4b5404896b6fdd60aa0101b5afd9d6
ViT <59057047+DOMO-Dom@users.noreply.github.com>
2022-08-31 14:55:13 +0800

    Added New Layouts

---
+++ app/src/main/java/com/example/sprout/activity/startup/GetStarted.java
@@ -0,0 +1,16 @@
+package com.example.sprout.activity.startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sprout.R;
+
+public class GetStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitty_startup_get_started);
+    }
+}
--- app/src/main/java/com/example/sprout/activity/startup/get/Identity.java
+++ app/src/main/java/com/example/sprout/activity/startup/get/Identity.java
@@ -2,15 +2,28 @@ package com.example.sprout.activity.startup.get;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sprout.R;
+import com.example.sprout.activity.startup.GetStarted;
 
 public class Identity extends AppCompatActivity {
+
+    Button btn_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup_get_identity);
+
+        btn_continue = (Button) findViewById(R.id.btn_continue);
+
+        btn_continue.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), GetStarted.class);
+            startActivity(intent);
+        });
     }
 }
