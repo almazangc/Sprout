17caa9301113c61c5a57c668dd36cacf0789ba16
almazan <gilbertalmazan61@gmail.com>
2022-11-22 23:29:29 +0800

    Refactored From Class to Enum

---
+++ app/src/main/java/com/habitdev/sprout/enums/BundleKeys.java
@@ -0,0 +1,22 @@
+package com.habitdev.sprout.enums;
+
+public enum BundleKeys {
+
+    EULA("EULA?KEY"),
+    WAKE_HOUR("WAKE_HOUR?KEY"),
+    WAKE_MINUTE("WAKE_MINUTE?KEY"),
+    SLEEP_HOUR("SLEEP_HOUR?KEY"),
+    SLEEP_MINUTE("SLEEP_MINUTE?KEY"),
+    NICKNAME("NICKNAME?KEY"),
+    ANALYSIS("ANALYSIS?KEY");
+
+    String KEY;
+
+    BundleKeys(String KEY){
+        this.KEY = KEY;
+    }
+
+    public String getKEY() {
+        return KEY;
+    }
+}
