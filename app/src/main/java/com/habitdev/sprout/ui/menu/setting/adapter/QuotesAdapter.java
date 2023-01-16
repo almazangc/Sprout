0c77a3d9d70d7f1bbbb242d1267f8657d9c53cc4
ViT <59057047+DOMO-Dom@users.noreply.github.com>
2023-01-17 01:13:40 +0800

    more commit

---
+++ app/src/main/java/com/habitdev/sprout/ui/menu/setting/adapter/QuotesAdapter.java
@@ -0,0 +1,171 @@
+package com.habitdev.sprout.ui.menu.setting.adapter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListUpdateCallback;
+
+import com.habitdev.sprout.R;
+import com.habitdev.sprout.database.quotes.model.Quotes;
+import com.habitdev.sprout.ui.menu.journal.adapter.JournalNoteItemAdapter;
+import com.habitdev.sprout.utill.NotesDiffUtil;
+import com.habitdev.sprout.utill.QuotesDiffUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class QuotesAdapter extends ArrayAdapter<Quotes> {
+
+    private List<Quotes> oldQuotesList;
+    private List<Quotes> originalNoteList;
+    private Timer timer;
+
+    public QuotesAdapter(@NonNull Context context, @NonNull List<Quotes> oldQuotesList) {
+        super(context, 0, oldQuotesList);
+        this.oldQuotesList = oldQuotesList;
+        this.originalNoteList = new ArrayList<>(oldQuotesList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.adapter_setting_terminal_quotes_item, parent, false);
+        }
+
+        Quotes quotes = oldQuotesList.get(position);
+        TextView quoted = itemView.findViewById(R.id.quotes_drop_item_quoted);
+        TextView author = itemView.findViewById(R.id.quotes_drop_item_author);
+
+        if (quotes != null) {
+            quoted.setText(quotes.getQuoted());
+            author.setText(quotes.getAuthor());
+        }
+
+        return itemView;
+    }
+
+    @Override
+    public int getCount() {
+        return oldQuotesList.size();
+    }
+
+    @Override
+    public Quotes getItem(int position) {
+        return oldQuotesList.get(position);
+    }
+
+    /**
+     * Filters the list of quotes based on the search key.
+     *
+     * @param keyword the search key
+     */
+    public void filter(final String keyword) {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (keyword.isEmpty()) {
+                    oldQuotesList = originalNoteList;
+                } else {
+                    List<Quotes> filteredList = new ArrayList<>();
+                    for (Quotes item : oldQuotesList) {
+                        if (item.getAuthor().toLowerCase().contains(keyword) || item.getQuoted().toLowerCase().contains(keyword)) {
+                            filteredList.add(item);
+                        }
+                    }
+                    oldQuotesList = filteredList;
+                }
+
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        DiffUtil.Callback DIFF_CALLBACK = new QuotesDiffUtil(originalNoteList, oldQuotesList);
+                        DiffUtil.DiffResult DIFF_CALLBACK_RESULT = DiffUtil.calculateDiff(DIFF_CALLBACK);
+                        DIFF_CALLBACK_RESULT.dispatchUpdatesTo(new ListUpdateCallback() {
+                            @Override
+                            public void onInserted(int position, int count) {
+                                notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onRemoved(int position, int count) {
+                                notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onMoved(int fromPosition, int toPosition) {
+                                notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onChanged(int position, int count, @Nullable Object payload) {
+                                notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
+            }
+        }, 500);
+
+        notifyDataSetChanged();
+    }
+
+    public void cancelTimer() {
+        if (timer != null) timer.cancel();
+    }
+
+    /**
+     * Updates the list of quotes with new quotes and animates the changes.
+     *
+     * @param newQuotesList the new list of quotes
+     */
+    public void setNewQuotesList(List<Quotes> newQuotesList) {
+        DiffUtil.Callback DIFF_CALLBACK = new QuotesDiffUtil(oldQuotesList, newQuotesList);
+        DiffUtil.DiffResult DIFF_CALLBACK_RESULT = DiffUtil.calculateDiff(DIFF_CALLBACK);
+
+        DIFF_CALLBACK_RESULT.dispatchUpdatesTo(new ListUpdateCallback() {
+            @Override
+            public void onInserted(int position, int count) {
+                oldQuotesList = newQuotesList;
+                originalNoteList = oldQuotesList;
+//                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onRemoved(int position, int count) {
+                oldQuotesList = newQuotesList;
+                originalNoteList = oldQuotesList;
+//                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onMoved(int fromPosition, int toPosition) {
+                oldQuotesList = newQuotesList;
+                originalNoteList = oldQuotesList;
+//                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChanged(int position, int count, @Nullable Object payload) {
+                oldQuotesList = newQuotesList;
+                originalNoteList = oldQuotesList;
+//                notifyDataSetChanged();
+            }
+        });
+    }
+}
--- app/src/main/java/com/habitdev/sprout/ui/menu/setting/ui/TerminalFragment.java
+++ app/src/main/java/com/habitdev/sprout/ui/menu/setting/ui/TerminalFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
@@ -17,13 +16,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.habitdev.sprout.R;
 import com.habitdev.sprout.database.quotes.QuotesViewModel;
 import com.habitdev.sprout.database.quotes.model.Quotes;
 import com.habitdev.sprout.databinding.FragmentTerminalBinding;
+import com.habitdev.sprout.ui.menu.setting.adapter.QuotesAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,6 +32,7 @@ public class TerminalFragment extends Fragment {
     private static QuotesViewModel quotesViewModel;
     private static final List<Quotes>[] quotesList = new List[]{new ArrayList<>()};
     private static int selectedItemPos = -1; // default none selected item
+    private static QuotesAdapter quotesAdapter;
 
     public interface OnReturnSetting {
         void returnFromTerminalToSetting();
@@ -47,7 +44,8 @@ public class TerminalFragment extends Fragment {
         this.mOnReturnSetting = mOnReturnSetting;
     }
 
-    public TerminalFragment() {}
+    public TerminalFragment() {
+    }
 
     @Nullable
     @Override
@@ -57,75 +55,7 @@ public class TerminalFragment extends Fragment {
 
         quotesViewModel = new ViewModelProvider(requireActivity()).get(QuotesViewModel.class);
 
-        //Get db ref
-        FirebaseFirestore firebaseFirestoreDB = FirebaseFirestore.getInstance();
-        setDropItems(firebaseFirestoreDB);
-
-        //Keys
-        final String QUOTED_KEY = "quoted";
-        final String AUTHOR_KEY = "author";
-
-        binding.save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String docID = binding.docID.getText().toString().trim();
-                final String quoted = binding.quoted.getText().toString().trim();
-                final String author = binding.author.getText().toString().trim();
-
-                //create map obj
-                Map<String, Object> quotesMap = new HashMap<>(); //impletation of map interface
-                quotesMap.put(QUOTED_KEY, quoted);
-                quotesMap.put(AUTHOR_KEY, author);
-
-                if (selectedItemPos == -1) {
-                    quotesViewModel.insert_quotes(docID, quotesMap);
-                    Toast.makeText(requireActivity(), "Inserted", Toast.LENGTH_SHORT).show();
-                }
-
-                if (selectedItemPos >= 0 && selectedItemPos < quotesList[0].size()) {
-                    Quotes quotes = quotesList[0].get(selectedItemPos);
-                    quotes.setQuoted(quoted);
-                    quotes.setAuthor(author);
-                    quotesViewModel.update_quotes(quotes);
-                    Toast.makeText(requireActivity(), "Updated", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        binding.delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String docID = binding.docID.getText().toString().trim();
-                if (selectedItemPos == -1) {
-                    Toast.makeText(requireActivity(), "Item is not found dabase", Toast.LENGTH_SHORT).show();
-                }
-                if (selectedItemPos >= 0 && selectedItemPos < quotesList[0].size()) {
-                    quotesViewModel.delete_quotes(quotesList[0].get(selectedItemPos).getId());
-                    quotesList[0].remove(quotesList[0].get(selectedItemPos));
-                    selectedItemPos = -1;
-                    Toast.makeText(requireContext(), "Deleted", Toast.LENGTH_SHORT).show();
-                    binding.docID.setText("");
-                    binding.docID.setEnabled(true);
-                    binding.quoted.setText("");
-                    binding.author.setText("");
-                    binding.quotesDropItem.setText("");
-                    binding.save.setText("Save");
-                }
-            }
-        });
-
-        binding.clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(requireContext(), "Cleared", Toast.LENGTH_SHORT).show();
-                binding.docID.setText("");
-                binding.docID.setEnabled(true);
-                binding.quoted.setText("");
-                binding.author.setText("");
-                binding.quotesDropItem.setText("");
-                binding.save.setText("Save");
-            }
-        });
+        setDropItems();
 
         binding.quotesDropItem.addTextChangedListener(new TextWatcher() {
             @Override
@@ -151,26 +81,103 @@ public class TerminalFragment extends Fragment {
             }
         });
 
+        //Keys
+        final String QUOTED_KEY = "quoted";
+        final String AUTHOR_KEY = "author";
+
+        binding.save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String docID = binding.docID.getText().toString().trim();
+                final String quoted = binding.quoted.getText().toString().trim();
+                final String author = binding.author.getText().toString().trim();
+
+                //create map obj
+                Map<String, Object> quotesMap = new HashMap<>(); //impletation of map interface
+                quotesMap.put(QUOTED_KEY, quoted);
+                quotesMap.put(AUTHOR_KEY, author);
+
+                if (selectedItemPos == -1) {
+                    quotesViewModel.insert_quotes(docID, quotesMap);
+                    Toast.makeText(requireActivity(), "Inserted", Toast.LENGTH_SHORT).show();
+                    notifyDataSetChanged();
+                }
+
+                if (selectedItemPos >= 0 && selectedItemPos < quotesList[0].size()) {
+                    Quotes quotes = quotesList[0].get(selectedItemPos);
+                    quotes.setQuoted(quoted);
+                    quotes.setAuthor(author);
+                    quotesViewModel.update_quotes(quotes);
+                    Toast.makeText(requireActivity(), "Updated", Toast.LENGTH_SHORT).show();
+                    notifyDataSetChanged();
+                }
+            }
+        });
+
+        binding.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedItemPos == -1) {
+                    Toast.makeText(requireActivity(), "Item is not found dabase", Toast.LENGTH_SHORT).show();
+                }
+                if (selectedItemPos >= 0 && selectedItemPos < quotesList[0].size()) {
+                    quotesViewModel.delete_quotes(quotesList[0].get(selectedItemPos).getId());
+                    quotesList[0].remove(quotesList[0].get(selectedItemPos));
+                    selectedItemPos = -1;
+                    Toast.makeText(requireContext(), "Deleted", Toast.LENGTH_SHORT).show();
+                    binding.docID.setText("");
+                    binding.docID.setEnabled(true);
+                    binding.quoted.setText("");
+                    binding.author.setText("");
+                    binding.quotesDropItem.setText("");
+                    binding.save.setText("Save");
+                    notifyDataSetChanged();
+                }
+
+            }
+        });
+
+        binding.clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "Cleared", Toast.LENGTH_SHORT).show();
+                binding.docID.setText("");
+                binding.docID.setEnabled(true);
+                binding.quoted.setText("");
+                binding.author.setText("");
+                binding.quotesDropItem.setText("");
+                binding.save.setText("Save");
+                notifyDataSetChanged();
+            }
+        });
+
         return binding.getRoot();
     }
 
-    private void setDropItems(FirebaseFirestore firebaseFirestoreDB) {
+    private void notifyDataSetChanged() {
+        if (quotesAdapter != null && binding.quotesDropItem.getAdapter() != null) {
+            quotesAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void setDropItems() {
         quotesViewModel.fetchData();
         quotesList[0] = quotesViewModel.getData();
 
         List<String> quotesID = new ArrayList<>();
-        for (Quotes quotes : quotesList[0]){
+        for (Quotes quotes : quotesList[0]) {
             quotesID.add(quotes.getQuoted());
         }
 
-        ArrayAdapter<String> adapterItems = new ArrayAdapter<>(requireContext(), R.layout.adapter_setting_terminal_quotes_item, quotesID); // handle config changes
-        binding.quotesDropItem.setAdapter(adapterItems);
+        quotesAdapter = new QuotesAdapter(requireContext(), quotesList[0]);
+        binding.quotesDropItem.setAdapter(quotesAdapter);
 
         binding.quotesDropItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 selectedItemPos = position;
                 Quotes quotes = quotesList[0].get(position);
+                binding.quotesDropItem.setText(quotesList[0].get(position).getQuoted());
                 binding.docID.setText(quotes.getId());
                 binding.docID.setEnabled(false);
                 binding.quoted.setText(quotes.getQuoted());
@@ -179,59 +186,33 @@ public class TerminalFragment extends Fragment {
             }
         });
 
+        binding.quotesDropItemTextInput.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                quotesAdapter.cancelTimer();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!quotesList[0].isEmpty()) {
+                    quotesAdapter.filter(editable.toString().trim().toLowerCase());
+                }
+            }
+        });
+
         quotesViewModel.getLiveData().observe(getViewLifecycleOwner(), new Observer<List<Quotes>>() {
             @Override
             public void onChanged(List<Quotes> quotes) {
                 quotesViewModel.fetchData();
+                quotesAdapter.setNewQuotesList(quotes);
                 quotesList[0] = quotes;
             }
         });
-
-//        firebaseFirestoreDB.collection("quotes").get(Source.SERVER);
-//        firebaseFirestoreDB.collection("quotes")
-//                .get(Source.CACHE)
-//                .addOnSuccessListener(requireActivity(), new OnSuccessListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onSuccess(QuerySnapshot documents) {
-//                        if (documents != null) {
-//                            List<String> quotes_doc_id = new ArrayList<>();
-//                            List<Quotes> quotesList = documents.toObjects(Quotes.class);
-//
-//                            if (!quotesList.isEmpty()) {
-//                                for (int pos = 0; pos < quotesList.size(); pos++) {
-//                                    String documentID = documents.getDocuments().get(pos).getId();
-//                                    quotes_doc_id.add(documentID);
-//                                }
-//                                ArrayAdapter<String> adapterItems = new ArrayAdapter<>(requireContext(), R.layout.adapter_setting_terminal_quotes_item, quotes_doc_id); // handle config changes
-//                                binding.quotesDropItem.setAdapter(adapterItems);
-//                                Toast.makeText(requireContext(), "new adapteritems: " + quotes_doc_id.size(), Toast.LENGTH_SHORT).show();
-//
-//                                binding.quotesDropItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                                    @Override
-//                                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-//                                        Quotes quotes = quotesList.get(position);
-//                                        String documentID = documents.getDocuments().get(position).getId();
-//                                        binding.docID.setText(documentID);
-//                                        binding.docID.setEnabled(false);
-//                                        binding.quoted.setText(quotes.getQuoted());
-//                                        binding.author.setText(quotes.getAuthor());
-//                                    }
-//                                });
-//                            } else {
-//                                //do nothing for now
-//                            }
-//                        } else {
-//                            Log.d("tag", "SplashScreen: onSuccess() called: null documents ");
-//                            firebaseFirestoreDB.collection("quotes").get(Source.SERVER);
-//                        }
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.d("tag", "SplashScreen: onFailure() called: Data is not available on CACHE");
-//                    }
-//                });
     }
 
     private void onBackPress() {
@@ -241,6 +222,7 @@ public class TerminalFragment extends Fragment {
                 if (mOnReturnSetting != null) {
                     mOnReturnSetting.returnFromTerminalToSetting();
                 }
+                //detach observer maybe
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
@@ -250,5 +232,10 @@ public class TerminalFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
