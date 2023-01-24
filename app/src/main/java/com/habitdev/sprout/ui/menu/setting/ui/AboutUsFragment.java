a30fe2e80fe46bb3d54b5cd6974f8191915e416d
ViT <59057047+DOMO-Dom@users.noreply.github.com>
2023-01-24 18:49:34 +0800

    morecommit

--- app/src/main/java/com/habitdev/sprout/ui/menu/setting/ui/AboutUsFragment.java
+++ app/src/main/java/com/habitdev/sprout/ui/menu/setting/ui/AboutUsFragment.java
@@ -1,16 +1,25 @@
 package com.habitdev.sprout.ui.menu.setting.ui;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 
 import com.habitdev.sprout.databinding.FragmentAboutUsBinding;
+import com.habitdev.sprout.utill.NetworkMonitoringUtil;
+import com.habitdev.sprout.utill.NetworkStateManager;
 
 public class AboutUsFragment extends Fragment {
 
@@ -32,11 +41,68 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentAboutUsBinding.inflate(inflater, container, false);
+
+        binding.sample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFacebookProfile("https://www.facebook.com/gcalmazan/");
+            }
+        });
         onBackPress();
         return binding.getRoot();
     }
 
-    private void onBackPress() {
+
+    /**
+     * Opens a Facebook profile link using the Facebook app if it is installed, or the device's default browser if it is not.
+     * @param profileUrl The Facebook profile link to be opened.
+     */
+    public void openFacebookProfile(String profileUrl) {
+        ConnectivityManager connectivityManager;
+        NetworkMonitoringUtil networkMonitoringUtil;
+
+        connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkMonitoringUtil = new NetworkMonitoringUtil(getContext());
+
+        Intent facebookIntent;
+        NetworkStateManager networkStateManager = NetworkStateManager.getInstance();
+        LiveData<Boolean> networkConnectivityStatus = networkStateManager.getNetworkConnectivityStatus();
+        boolean isNetworkConnected = networkConnectivityStatus.getValue() == null ? false : networkConnectivityStatus.getValue();
+        if (isNetworkConnected) {
+            try {
+                // Try to open the link in the Facebook app
+                facebookIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://facewebmodal/f?href=" + profileUrl));
+                startActivity(facebookIntent);
+                // unregister the network callback
+                if (connectivityManager != null && networkMonitoringUtil != null) {
+                    connectivityManager.unregisterNetworkCallback(networkMonitoringUtil);
+                }
+            } catch (ActivityNotFoundException e) {
+                // If the Facebook app is not installed, open the link in the device's default browser
+                facebookIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(profileUrl));
+                startActivity(facebookIntent);
+                // unregister the network callback
+                if (connectivityManager != null && networkMonitoringUtil != null) {
+                    connectivityManager.unregisterNetworkCallback(networkMonitoringUtil);
+                }
+            } catch (Exception e) {
+                // Handle any other exceptions
+                Toast.makeText(getContext(), "An error occurred while trying to open the Facebook profile.", Toast.LENGTH_SHORT).show();
+                // unregister the network callback
+                if (connectivityManager != null && networkMonitoringUtil != null) {
+                    connectivityManager.unregisterNetworkCallback(networkMonitoringUtil);
+                }
+            }
+        } else {
+            Toast.makeText(getContext(), "No internet connection available", Toast.LENGTH_SHORT).show();
+            connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkMonitoringUtil = new NetworkMonitoringUtil(getContext());
+            networkMonitoringUtil.registerNetworkCallbackEvents();
+        }
+    }
+
+
+            private void onBackPress() {
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
